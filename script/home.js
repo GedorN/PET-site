a04828bf788dfcd4e629db2f7235e35f66eb1889
GedorN <gedor.silvaneto@gmail.com>
2018-11-11 02:02:38 -0200

    Changing colors

--- script/home.js
+++ script/home.js
@@ -74,7 +74,6 @@ function mouseon(id)
 					break;
 			}
 			break;
-
 		default:
 			alert("Quem você mandou?");
 			break;
@@ -148,4 +147,144 @@ function mouseout(id)
 	}
 }
 
+function change_contact_a()
+{
+	var c= document.getElementById("contact_a");
+	switch(how_is_active)
+	{
+		case "red":
+			c.src="./img/contato_rosa.png";
+			break;
+		case "orange":
+			c.src="./img/contato_laranja.png";
+			break;
+
+		case "blue":
+			c.src="./img/contato_roxo.png";
+			break;
+
+		default:
+			c.src="./img/contato_roxo.png";
+			break;
+	}
+}
+function change_instagram_a()
+{
+	var c= document.getElementById("instagram_a");
+	switch(how_is_active)
+	{
+		case "red":
+			c.src="./img/instagram_rosa.png";
+			break;
+		case "orange":
+			c.src="./img/instagram_laranja.png";
+			break;
+
+		case "blue":
+			c.src="./img/instagram_roxo.png";
+			break;
+
+		default:
+			c.src="./img/instagram_roxo.png";
+			break;
+	}
+}
+function change_facebook_a()
+{
+	var c= document.getElementById("facebook_a");
+	switch(how_is_active)
+	{
+		case "red":
+			c.src="./img/facebook_rosa.png";
+			break;
+		case "orange":
+			c.src="./img/facebook_laranja.png";
+			break;
+
+		case "blue":
+			c.src="./img/facebook_roxo.png";
+			break;
+
+		default:
+			c.src="./img/facebook_roxo.png";
+			break;
+	}
+}
+function chage_menu()
+{
+	var c= document.getElementById("menu_a");
+	switch(how_is_active)
+	{
+		case "red":
+			c.src="./img/menu_rosa.png";
+			break;
+		case "orange":
+			c.src="./img/menu_laranja.png";
+			break;
+
+		case "blue":
+			c.src="./img/menu_roxo.png";
+			break;
+
+		default:
+			c.src="./img/menu_roxo.png";
+			break;
+	}
+}
+
+function change_text_box()
+{
+	var c= document.getElementById("text-box");
+	switch(how_is_active)
+	{
+		case "red":
+			c.style.backgroundColor= "#e53648";
+			break;
+		case "orange":
+			c.style.backgroundColor= "#f86624";
+			break;
+
+		case "blue":
+			c.style.backgroundColor= "#662e9b";
+			break;
+
+		default:
+			c.style.backgroundColor= "#662e9b";
+			break;
+	}
+}
+function change_home_photo()
+{
+	var c= document.getElementById("home_photo");
+	switch(how_is_active)
+	{
+		case "red":
+			c.src="./img/img_home_vermelho.png";
+			break;
+		case "orange":
+			c.src="./img/img_home_laranja.png";
+			break;
+
+		case "blue":
+			c.src="./img/img_home_roxo.png";
+			break;
+
+		default:
+			c.src="./img/img_home_roxo.png";
+			break;
+	}
+}
+
+function changeColors()
+{
+	console.log("Function changeColors()");	
+	
+	change_contact_a();
+	change_instagram_a();
+	change_facebook_a();
+	chage_menu();
+	change_home_photo();
+	change_text_box();
+
+}
 
